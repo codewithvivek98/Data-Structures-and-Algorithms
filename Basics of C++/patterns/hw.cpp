df86387ee4a215271c58a4ed08d949250582e543
vivek kumar soni <vivek.soni9891@gmail.com>
2026-07-26 01:35:41 +0530

    question on pattern

--- Basics of C++/patterns/hw.cpp
+++ Basics of C++/patterns/hw.cpp
@@ -27,13 +27,54 @@ int main() {
     // } 
 
 ///////
-    int n = 4;
-    char ch = 'A';
-    for(int i=0; i<=n-1; i++) {
-        for(int j=1; j<=i+1; j++) {
-            cout << ch << " ";
+    // int n = 4;
+    // char ch = 'A';
+    // for(int i=0; i<=n-1; i++) {
+    //     for(int j=1; j<=i+1; j++) {
+    //         cout << ch << " ";
+    //     }
+    //     ch++;
+    //     cout << endl;
+    // }
+
+
+   // // butterfly pattern
+    //upper half
+    int n =4;
+    for(int i=1; i<=n; i++) {
+
+        //left start
+        for(int j=1; j<=i; j++) {
+            cout << "*";
         }
-        ch++;
+
+        //space
+        for(int j=1; j<=2*(n-i); j++) {
+            cout << " ";
+        }
+
+        //Right star
+        for(int j=1; j<=i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    ////lower half
+    for(int i=n; i>=1; i--) {
+        //left start
+        for(int j=1; j<=i; j++) {
+            cout << "*";
+        }
+
+        //space
+        for(int j=1; j<=2*(n-i); j++) {
+            cout << " ";
+        }
+
+        //right star
+        for(int j=1; j<=i; j++) {
+            cout << "*";
+        }
         cout << endl;
     }
 
