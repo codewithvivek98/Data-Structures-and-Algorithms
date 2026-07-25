c56c586f9e32a8474a8ac569efd4d0e3810e6b64
vivek kumar soni <vivek.soni9891@gmail.com>
2026-07-26 01:05:06 +0530

    loops

--- Basics of C++/patterns/code.cpp
+++ Basics of C++/patterns/code.cpp
@@ -49,7 +49,6 @@ int main() {
 // hw1
 
 
-
 // 3. Triangle pattern ,, right angle triangle
 // int n = 4;
 // for(int i=0; i<n; i++) {
@@ -59,6 +58,7 @@ int main() {
 //     cout << endl;
 // }
 
+
 // triangle for numbers --> repeating 
 // int n = 4;
 // for(int i=0; i<n; i++) {
@@ -67,6 +67,7 @@ int main() {
 //     }
 //     cout << endl;
 // }
+
 
 // int n = 4;
 // for(int i=0; i<=n-1; i++) {
@@ -78,15 +79,127 @@ int main() {
 
 
 // reverse triangle pattern
-int n = 4;
-for(int i=0; i<n; i++) {
-   for(int j=i+1; j>0; j--) {
-    cout << j << " ";
-   }
-   cout << endl;
-}
+// int n = 4;
+// for(int i=0; i<n; i++) {
+//    for(int j=i+1; j>0; j--) {
+//     cout << j << " ";
+//    }
+//    cout << endl;
+// }
 
 
+// Floyd's Triangle pattern
+// int n = 4;
+// int num = 1;
+// for(int i=0; i<n; i++) {
+//     for(int j=0; j<i+1; j++) {
+//         cout << num << " ";
+//         num++;
+//     }
+//     cout << endl;
+// }
+// int n = 4;
+// char ch = 'A';
+// for(int i=0; i<n; i++) {
+//     for(int j=0; j<i+1; j++) {
+//         cout << ch << " ";
+//         ch++;
+//     }
+//     cout << endl;
+// }
 
+
+//Inverted triangle pattern
+// int n = 4;
+// for(int i=0; i<n; i++) {
+
+
+//     //space
+//     for(int j=0; j<i; j++) {
+//         cout << " "; 
+//     }
+
+
+//     // numbers
+//     for(int j=0; j<n-i; j++) {
+//         cout << (i+1);
+//     }
+//     cout << endl;
+// }
+
+
+//printing characters for above 
+// int n = 4;
+// char ch = 'A';
+// for(int i=0; i<n; i++) {
+//     //space
+//     for(int j=0; j<i; j++) {
+//         cout << " ";
+//     }
+//     //characters
+//     for(int j=0; j<n-i; j++) {
+//         cout << ch; 
+//     }
+//     ch++;
+//     cout << endl;
+// }
+
+
+// Pyramid pattern
+// int n = 4;
+// //outer loop
+// for(int i=0; i<n; i++) {
+//     //space n-i-1
+//     for(int j=0; j<n-i-1; j++) {
+//         cout << " ";
+//     } 
+//     //num1
+//     for(int j=1; j<=i+1; j++) {
+//         cout << j;
+//     }
+//     //num2
+//     for(int j=i; j>=1; j--) {
+//         cout << j;
+//     }
+//     cout << endl;
+// }
+
+
+// Hollow Diamond Pattern
+// int n = 4;
+// //for top part
+// for(int i=0; i<n; i++) {
+//     //space n-i-1
+//     for(int j=0; j<n-i-1; j++) {
+//         cout << " ";
+//     }
+//     cout << "*";
+//     //condition
+//     if(i != 0) {
+//         //space
+//         for(int j=0; j<2*i-1; j++) {
+//             cout << " ";
+//         }
+//         cout << "*";
+//     }
+//     cout << endl;
+// }
+// //bottom part
+// for(int i = 0; i < n-1; i++) {
+//     // spaces
+//     for(int j = 0; j < i+1; j++) {
+//         cout << " ";
+//     }
+//     cout << "*";
+//     if(i != n-2) {
+//         for(int j = 0; j < 2*(n-i)-5; j++) {
+//             cout << " ";
+//         }
+//         cout << "*";
+//     }
+//     cout << endl;
+// }
+  
     return 0;
+
 }
